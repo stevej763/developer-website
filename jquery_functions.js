6da7a530f11c29258388e4bc4c91530e2d1a7715
Steve Jones <steve.jones763@gmail.com>
2020-09-15 21:12:45 +0100

    css styling changes and images added

--- jquery_functions.js
+++ jquery_functions.js
@@ -1,8 +1,14 @@
 
 //smooth scroll to href
 $(document).on('click', 'a[href^="#"]', function(event) {
-  event.preventDefault();
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 500);
+    //smooth slide to target unless it is the carousel slider buttons, then ignore
+    if (event.target.getAttribute('id') != 'c-slider') {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 500);
+    };
+
+
+
 });
